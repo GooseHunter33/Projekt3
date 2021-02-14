89e5a761885ea1b3f32bc4a318b30b9aa226684f
GooseHunter33 <markus.eriksson@elev.ga.ntig.se>
2021-02-14 15:51:52 +0100

    blev klar med footern, fixade en estetisk grid till spelet och andra småfixar.

---
+++ main.js
@@ -0,0 +1,15 @@
+let battlefield = []
+[1, 0, 0, 0, 0, 0, 0, 0];
+[0, 0, 0, 0, 0, 0, 0, 0];
+[0, 0, 0, 0, 0, 0, 0, 0];
+[0, 0, 0, 0, 0, 0, 0, 0];
+[0, 0, 0, 0, 0, 0, 0, 0];
+[0, 0, 0, 0, 0, 0, 0, 0];
+[0, 0, 0, 0, 0, 0, 0, 0];
+[0, 0, 0, 0, 0, 0, 0, 0];
+
+let positions = document.querySelectorAll(".skepp-grid button");
+for( i = 0;i<positions.length;i++ );
+{
+    
+}
