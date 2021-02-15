3260f50ae06f6fd466d8cb68f5176fdc350ec79b
GooseHunter33 <markus.eriksson@elev.ga.ntig.se>
2021-02-15 17:25:08 +0100

    (lärforum + lite hemma) Fixat med javascriptet

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
-let battlefield = []
-[1, 0, 0, 0, 0, 0, 0, 0];
-[0, 0, 0, 0, 0, 0, 0, 0];
-[0, 0, 0, 0, 0, 0, 0, 0];
-[0, 0, 0, 0, 0, 0, 0, 0];
-[0, 0, 0, 0, 0, 0, 0, 0];
-[0, 0, 0, 0, 0, 0, 0, 0];
-[0, 0, 0, 0, 0, 0, 0, 0];
-[0, 0, 0, 0, 0, 0, 0, 0];
+let battlefield = [];
 
 let positions = document.querySelectorAll(".skepp-grid button");
-for( i = 0;i<positions.length;i++ );
-{
-    
+
+function generateBoard(width, height, numShipsToPlace) {
+  let grid = document.querySelector(".skepp-grid");
+  let shipsPlaced = 0;
+  grid.style.gridTemplateColumns = "repeat(" + width + ", 1fr)";
+  for (let y = 0; y < height; y++) {
+    battlefield.push([]);
+    for (let x = 0; x < width; x++) {
+      // <button class="location-btn" data-coordinate="0,0" type="button"></button>
+      if (shipsPlaced < numShipsToPlace) {
+        if (Math.random() < 0.25) {
+          battlefield[y].push(1);
+          shipsPlaced++;
+        } else {
+          battlefield[y].push(0);
+        }
+      } else {
+        battlefield[y].push(0);
+      }
+      let newButton = document.createElement("button");
+      newButton.classList.add("location-btn");
+      newButton.setAttribute("data-coordinate", x + "," + y);
+      newButton.setAttribute("type", "button");
+      newButton.addEventListener("click", testLocation);
+      grid.append(newButton);
+    }
+  }
+  console.log(battlefield);
 }
+
+function testLocation(event) {
+  let coordinate = event.target.getAttribute("data-coordinate").split(",");
+  let x = coordinate[0];
+  let y = coordinate[1];
+
+  console.log(battlefield[x][y]);
+
+  if (battlefield[x][y] != 1) {
+    console.log("No ship");
+    window.alert("hummer");
+  } else {
+    console.log("HIT!");
+  }
+}
+
+generateBoard(8, 8, 10);
